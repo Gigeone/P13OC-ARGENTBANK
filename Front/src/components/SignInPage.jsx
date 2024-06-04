a620d2ab7e87a360f319a1f73649881ae7575878
Gigeone <matthieugh@gmail.com>
2024-06-04 14:43:42 +0200

    Profil Page and Sign In Page Integration

--- Front/src/components/SignInPage.jsx
+++ Front/src/components/SignInPage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Navbar from "./NavBar";
+import Footer from "./Footer";
 
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return (
+    <div>
+      <Navbar />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            {/* Placeholder link */}
+            <a href="./user.html" className="sign-in-button">
+              Sign In
+            </a>
+            {/* Uncomment the button below when converting to React */}
+            {/* <button className="sign-in-button">Sign In</button> */}
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default SignInPage;
